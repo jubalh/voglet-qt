088647fadf42025d8c1eeb9bb7f24323012bfa45
Michael Vetter <g.bluehut@gmail.com>
2013-05-12 10:56:45 +0200

    style

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     if ( !conf->load() )
         return -1;
 
-    CVoglet *v = new CVoglet( conf);
+    CVoglet *v = new CVoglet(conf);
 
     CEntry *entry;
     entry = v->getEntryByRandom();
