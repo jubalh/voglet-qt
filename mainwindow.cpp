4692e24188d31ae1aaffbc0946a75b6530361a03
Michael Vetter <g.bluehut@gmail.com>
2013-05-12 14:29:52 +0200

    handle key presses. compare input with correct answer.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <QLabel>
+#include <QRect>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,6 +8,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->leWord->installEventFilter(this);
+    ui->leTranslation->installEventFilter(this);
     this->config = 0;
 }
 
@@ -37,19 +41,20 @@ void MainWindow::setTranslation(CEntry *entry)
 
 void MainWindow::setEntry(CEntry *entry)
 {
+    this->entry = entry;
     if ( this->config )
     {
-        VOGLET_CONFIG_DIRECTION direction = this->config->getDirection();
-        if (direction == VOGLET_CONFIG_DIRECTION_BOTH)
+        this->lastDirection = this->config->getDirection();
+        if (this->lastDirection== VOGLET_CONFIG_DIRECTION_BOTH)
         {
             int t = qrand() % 2;
             if (t==0)
-                direction = VOGLET_CONFIG_DIRECTION_TRANSLATION;
+                this->lastDirection = VOGLET_CONFIG_DIRECTION_TRANSLATION;
             else
-                direction = VOGLET_CONFIG_DIRECTION_WORD;
+               this->lastDirection = VOGLET_CONFIG_DIRECTION_WORD;
         }
 
-        if (direction == VOGLET_CONFIG_DIRECTION_TRANSLATION)
+        if (this->lastDirection == VOGLET_CONFIG_DIRECTION_TRANSLATION)
             this->setTranslation(entry);
         else
             this->setWord(entry);
@@ -59,4 +64,49 @@ void MainWindow::setEntry(CEntry *entry)
         ui->leWord->setText(entry->word);
         ui->leTranslation->setText(entry->translation);
     }
+}
+
+void MainWindow::compareInput(QLineEdit *edit, QString &text)
+{
+    if (edit->text().compare(text,Qt::CaseSensitive))
+    {
+        QRect rect;
+        QLabel *lbl = new QLabel(this);
+
+        rect.setX(edit->x() + edit->frameGeometry().width()+40);
+        rect.setY(ui->leWord->y());
+        rect.setWidth(50);
+        rect.setHeight(20);
+        lbl->setGeometry(rect);
+
+        lbl->setText(text);
+        lbl->setBuddy(edit);
+        lbl->setStyleSheet("QLabel {color: red}");
+
+        lbl->setVisible(true);
+    }
+    else
+        this->close();
+}
+
+bool MainWindow::eventFilter(QObject *object, QEvent *event)
+{
+    if (event->type() == QEvent::KeyPress)
+    {
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+        switch(keyEvent->key())
+        {
+        case Qt::Key_Escape:
+            this->close();
+            break;
+        case Qt::Key_Enter:
+        case Qt::Key_Return:
+            if (this->lastDirection == VOGLET_CONFIG_DIRECTION_TRANSLATION)
+                compareInput(ui->leWord, this->entry->word);
+            else
+                compareInput(ui->leTranslation, this->entry->translation);
+            return true;
+        }
+    }
+    return QMainWindow::eventFilter(object, event);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QKeyEvent>
+#include <QLineEdit>
 #include "entry.h"
 #include "config.h"
 
@@ -20,9 +22,13 @@ public:
     void setConfig(CConfig *config);
 private:
     Ui::MainWindow *ui;
+    CEntry *entry;
     CConfig *config;
+    VOGLET_CONFIG_DIRECTION lastDirection;
     void setTranslation(CEntry *entry);
     void setWord(CEntry *entry);
+    void compareInput(QLineEdit *edit, QString &text);
+    bool eventFilter(QObject *object, QEvent *event);
 };
 
 #endif // MAINWINDOW_H
